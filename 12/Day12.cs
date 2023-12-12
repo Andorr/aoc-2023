16ed1c14ffdf6a540457aa896e3e2559147d5eae
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2023-12-12 11:41:07 +0100

    12: refactored

--- 12/Day12.cs
+++ 12/Day12.cs
@@ -3,91 +3,73 @@ var input = parse("input.txt");
 Console.WriteLine($"Part01: {part01(input)}");
 Console.WriteLine($"Part02: {part02(input)}");
 
-long part01(List<SpringGroup> input)
-{
-    return input.Select((springGroup, i) =>
-        arragements(springGroup.s, springGroup.info)
-    ).Sum();
-}
+const char DAMAGED = '#';
+const char UNKNOWN = '?';
 
-long part02(List<SpringGroup> input)
-{
-    return input
-    .Select((springGroup) =>
-    {
-        return new SpringGroup(
-            string.Join("?", Enumerable.Range(0, 5).Select(i => springGroup.s)),
-            Enumerable.Range(0, 5).Select(i => springGroup.info).SelectMany(x => x).ToList()
-        );
-    })
+long part01(List<SpringGroup> input) => input
     .Select((springGroup, i) =>
-         arragements(springGroup.s, springGroup.info)
+        arragements(springGroup.s, springGroup.groupSizes)
     ).Sum();
-}
 
-long arragements(String springs, List<int> info, int groupIndex = 0, int i = 0, Dict<(int, int), long>? dp = null)
+long part02(List<SpringGroup> input) => input
+    .Select((springGroup) => new SpringGroup(
+            string.Join("?", Enumerable.Repeat(springGroup.s, 5).ToList()),
+            Enumerable.Repeat(springGroup.groupSizes, 5).SelectMany(x => x).ToList()
+        )
+    )
+    .Select((springGroup, i) =>
+         arragements(springGroup.s, springGroup.groupSizes)
+    ).Sum();
+
+long arragements(string springs, List<int> info, int groupIndex = 0, int i = 0, Dict<(int, int), long>? cache = null)
 {
-    if (dp == null)
-    {
-        dp = new Dict<(int, int), long>();
-    }
+    cache = cache ?? new Dict<(int, int), long>();
 
     if (groupIndex >= info.Count)
     {
-        return i >= springs.Length || springs.Substring(i).All(c => c != '#') ? 1 : 0;
+        return i >= springs.Length || springs.Substring(i).All(c => c != DAMAGED) ? 1 : 0;
     }
 
     long numArragements = 0;
-
     var groupSize = info[groupIndex];
-    while (i + groupSize <= springs.Length && groupIndex < info.Count)
+    while (i + groupSize <= springs.Length)
     {
-        if (dp.ContainsKey((groupIndex, i)))
+        if (cache.ContainsKey((groupIndex, i)))
         {
-            return dp[(groupIndex, i)];
+            return cache[(groupIndex, i)];
         }
 
-        var sub = springs.Substring(i, groupSize);
-
         // If i is a possible solution
-        if (sub.All(c => c == '#' || c == '?') &&
-            (i + groupSize >= springs.Length || springs[i + groupSize] != '#') &&
-            (i - 1 < 0 || springs[i - 1] != '#')
+        if (springs.Substring(i, groupSize).All(c => c == DAMAGED || c == UNKNOWN) &&
+            (i + groupSize >= springs.Length || springs[i + groupSize] != DAMAGED) &&
+            (i - 1 < 0 || springs[i - 1] != DAMAGED)
         )
         {
-            var num = arragements(springs, info, groupIndex + 1, i + groupSize + 1, dp);
-            dp[(groupIndex + 1, i + groupSize + 1)] = num;
+            var num = arragements(springs, info, groupIndex + 1, i + groupSize + 1, cache);
+            cache[(groupIndex + 1, i + groupSize + 1)] = num;
             numArragements += num;
         }
 
-        if (springs[i] == '#')
+        if (springs[i] == DAMAGED)
         {
-            // We can't skip a damaged
             break;
         }
 
         i++;
     }
-
     return numArragements;
 }
 
-List<SpringGroup> parse(string fileName)
-{
-    return File.ReadAllLines(fileName)
-        .Select(line =>
-        {
-            return line
-                .Split(" ")
-                .Let(comp =>
-                {
-                    var s = comp[0];
-                    var info = comp[1].Split(",").Select(int.Parse).ToList();
-                    return new SpringGroup(s, info);
-                });
-        })
-        .ToList();
-}
+List<SpringGroup> parse(string fileName) => File
+    .ReadAllLines(fileName)
+    .Select(line => line
+        .Split(" ")
+        .Let(comp => new SpringGroup(
+                comp[0],
+                comp[1].Split(",").Select(int.Parse).ToList()
+            )
+        )
+    )
+    .ToList();
 
-
-record SpringGroup(String s, List<int> info);
+record SpringGroup(String s, List<int> groupSizes);
